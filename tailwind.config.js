f0a4221a658c706f331fb16214fbfa7f9594085b
muhammad fiqri <muhfiqri033@google.com>
2024-09-24 22:33:54 +0800

    portofolio html tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+     fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [
+    
+  ],
+}
+
